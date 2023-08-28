ba1d0048527574a678f3e0ec9ab3f18835bddbc2
varkenvarken <andersmichel8@gmail.com>
2023-08-28 10:43:54 +0200

    documentation

--- coffeescraper/sftp.py
+++ coffeescraper/sftp.py
@@ -1,5 +1,30 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+"""
+Module for uploading files via SFTP using Paramiko.
+
+This module provides a function to upload files to a remote server using the SFTP protocol
+with the help of the Paramiko library. The function reads necessary connection information
+from secret files and performs the upload operation.
+
+Dependencies:
+    - paramiko
+    - .utils (from the same package)
+
+Functions:
+    upload_file_via_sftp(
+        hostfile: str,
+        usernamefile: str,
+        passwordfile: str,
+        local_file_path: str,
+        remote_file_path: str,
+    ) -> None:
+        Uploads a local file to a remote server via SFTP using provided connection details.
+        Skips the upload if DRYRUN environment variable is set.
+
+    Note: Ensure the necessary dependencies are installed before using this module.
+"""
+
 import paramiko
 
 from .utils import get_secret, get_env
@@ -12,11 +37,28 @@ def upload_file_via_sftp(
     local_file_path: str,
     remote_file_path: str,
 ) -> None:
-    
+    """
+    Upload a file to a remote server via SFTP.
+
+    This function uploads a local file to a remote server using the SFTP protocol.
+    It uses connection information provided through secret files, and it prints
+    a success message upon successful upload.
+
+    Args:
+        hostfile (str): Path to the secret file containing the host information.
+        usernamefile (str): Path to the secret file containing the username.
+        passwordfile (str): Path to the secret file containing the password.
+        local_file_path (str): Path to the local file to be uploaded.
+        remote_file_path (str): Path where the file should be uploaded on the remote server.
+
+    Returns:
+        None: This function does not return any value. It prints relevant messages.
+    """
+
     if get_env("DRYRUN") is not None:
         print(f"sftp upload of {local_file_path} skipped")
         return
-    
+
     host = get_secret(hostfile)
     username = get_secret(usernamefile)
     password = get_secret(passwordfile)
